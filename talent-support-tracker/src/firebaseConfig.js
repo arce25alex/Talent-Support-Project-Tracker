bc3033c43f9fff7b56ab4f87c14798559a01e6b6
arce25alex <arce25alex@gmail.com>
2025-03-11 17:26:57 -0400

    Add files via upload

--- talent-support-tracker/src/firebaseConfig.js
+++ talent-support-tracker/src/firebaseConfig.js
@@ -1,17 +1,14 @@
-// Import Firebase functions needed for authentication
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDv4eBCSNOvvu_UJPEFjfQa-giz4ImasoM",
-  authDomain: "talent-support-project-tracker.firebaseapp.com",
-  projectId: "talent-support-project-tracker",
-  storageBucket: "talent-support-project-tracker.appspot.com", 
-  messagingSenderId: "602478734856",
-  appId: "1:602478734856:web:5fce6e6d08076472a0abbd"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
